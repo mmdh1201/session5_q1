7935c8801d9ab60b498585dd22387ef53c3b0874
Mohammad Hosein Anvari <mh1383an1201@gmail.com>
2024-10-30 17:19:09 +0330

    [ADDED] Massage.cpp

---
+++ Massage.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include "Massage.h"
+using namespace std;
+User::User():name(" "),ID(" "){};
+User::User(string name , string ID): name(name),ID(ID){}
+User::~User(){}
+
+Date::Date():day(0),month(0),year(0){}
+Date::Date(int date){
+    day = date%100;
+    date/=100;
+    month = date%100;
+    date/=100;
+    year = date;
+}
+Date::~Date(){}
+
+BaseMessage::BaseMessage(){}
+BaseMessage::BaseMessage(int D, User U):d(Date(D)),user(U){}
+BaseMessage::~BaseMessage(){};
+
+SimpleMessage::SimpleMessage(){}
+SimpleMessage::SimpleMessage(string msg, User u, int date):msg(msg){this->setBaseAttr(u,date);}
+SimpleMessage::~SimpleMessage(){};
+
+PostMessage::PostMessage(){}
+PostMessage::PostMessage(string URL , User u, int date):imgPath(URL){
+    this->setBaseAttr(u,date);
+
+}
+PostMessage::~PostMessage(){}
+
+VoteMessage::VoteMessage(){}
+VoteMessage::VoteMessage(string vtitle, string* opt , User u , int date):Title(vtitle){
+    opt = new string[10];
+    for (int i=0;i<10;i++){
+        opt[i] = options[i];
+    }
+    this->setBaseAttr(u , date);
+}
+VoteMessage::~VoteMessage(){}
+
+quiz::quiz(int ans, User u, int date):Ans(ans){this->setBaseAttr(u,date);}
+quiz::~quiz(){}
+
+
+ostream& operator<< (ostream& out, const Date& d){
+    out<< d.day <<"/"<<d.month<<"/"<<d.year;
+    return out;
+}
+ostream& operator<< (ostream& out, const User& u){
+    out<< u.name <<"\n"<<u.ID<<endl;
+    return out;
+}
+
+void BaseMessage::printinfo(){
+    cout<<d<<endl;
+    cout<<user<<endl;
+}
+
+void SimpleMessage::setBaseAttr(User u , int date){
+    this->d = Date(date);
+    this->user = u;
+}
+void VoteMessage::setBaseAttr(User u , int date){
+    this->d = Date(date);
+    this->user = u;
+}
+
+void User::set_Id(int Id){
+    ID = Id;
+}
+string User::get_Id(){
+    return ID;
+}
+string BaseMessage::get_Id(){
+    return user.get_Id();
+}
+
+string quiz::answer(int in){
+    if (Ans==in){
+        return "True";
+    }else{
+        return "False";
+    }
+}
+
+void VoteMessage::print_op(){
+    for (int i=0; i<10;i++){
+        cout << options[i] << endl;
+    }
+}
+
+void quiz::set_optios(string* Options){
+    for (int i=0 ; i<10;i++){
+        this->options[i] = Options[i];
+    }
+}
+
+void quiz::set_attr(string t,int a,string* o){
+    this->Ans = a;
+    this->Title = t;
+    for (int i=0; i<10;i++){
+        options[i] = o[i];
+    }
+}
+
