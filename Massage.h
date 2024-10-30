b8ec9a18af62d82260d0bcdfba0b3c96d910d5f2
Mohammad Hosein Anvari <mh1383an1201@gmail.com>
2024-10-30 17:19:42 +0330

    [ADDED] Massage.h

---
+++ Massage.h
@@ -0,0 +1,93 @@
+#ifndef MASSAGE_H
+#define MASSAGE_H
+#include <iostream>
+#include<iostream>
+using namespace std;
+
+class User{
+    private:
+        string name;
+        string ID;
+    public:
+        friend ostream& operator<<(ostream&, const User&);
+        string get_Id();
+        void set_Id(int ID);
+        User();
+        User(string , string);
+        ~User();
+};
+
+
+class Date{
+    private:
+        int day;
+        int month;
+        int year;
+    public:
+        friend ostream& operator<<(ostream&, const Date&);
+        Date();
+        Date(int);
+        ~Date();
+};
+
+
+class BaseMessage
+{
+    protected:
+        Date d;
+        User user; 
+    public:
+        string get_Id();
+        BaseMessage();
+        BaseMessage(int , User);
+        ~BaseMessage();
+        void printinfo();
+};
+
+class SimpleMessage : public BaseMessage
+{
+    protected:
+    string msg;
+    public:
+    void setBaseAttr(User , int);
+    SimpleMessage();
+    SimpleMessage(string , User, int);
+    ~SimpleMessage();
+};
+
+class PostMessage : public SimpleMessage
+{
+    private:
+    string imgPath;
+    public:
+    PostMessage();
+    PostMessage(string , User, int);
+    ~PostMessage();
+};
+
+class VoteMessage : public BaseMessage
+{
+    protected: 
+    string Title;
+    string options[10];
+    public:
+    void setBaseAttr(User , int);
+    void print_op();
+    VoteMessage();
+    VoteMessage(string , string* , User, int);
+    ~VoteMessage();
+};
+
+class quiz:public VoteMessage
+{
+    private:
+        int Ans;
+    public:
+        quiz(int , User, int);
+        ~quiz();
+        void set_attr(string,int,string*);
+        void set_optios(string*);
+        string answer(int);
+};
+
+#endif
